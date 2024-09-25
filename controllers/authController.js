c2a7ae46b9640a833643aa034d59eb49a97b8a54
marksuit42 <158142252+marksuit42@users.noreply.github.com>
2024-09-25 18:10:17 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -73,3 +73,15 @@ exports.getCurrentUser = async (req, res) => {
     res.status(401).json({ message: 'Unauthorized' });
   }
 };
+
+
+exports.register = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const newUser = new User({ email, password });
+    await newUser.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error registering user' });
+  }
+};
